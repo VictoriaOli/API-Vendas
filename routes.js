9ef79224a7c6a295f08a794da0e823d3d8d8e41a
CalliopeM <66570495+CalliopeM@users.noreply.github.com>
2020-07-09 15:19:39 -0300

    Delete routes.js

--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controller = require('./controller');
-const routes = express.Router();
-
-
-routes.get('/vendas', controller.vendas);
-routes.post('/comprar', controller.comprar);
-
-module.exports = routes;
